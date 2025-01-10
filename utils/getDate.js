5307b249bc4d46d9fcbe6c77dad155a2341f5c2b
mattsnorris0 <mattsnorris0@gmail.com>
2025-01-10 15:30:21 -0600

    Delete all files

--- utils/getDate.js
+++
@@ -1,8 +0,0 @@
-export function getDate(timeStamp) {
-    let ISOStamp = new Date(timeStamp);
-    let date = ISOStamp.toLocaleDateString('en-us', {
-        month: 'numeric',
-        day: 'numeric',
-    });
-    return date;
-}
